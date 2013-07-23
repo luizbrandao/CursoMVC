9d009ad6d5ddfa4ab1ea1db7a965c6a46d29daf4
Luiz Brandao <luizao.cmd@gmail.com>
2013-07-23 17:48:47 -0400

    first commit

---
+++ CursoMVC/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CursoMVC.Models;
+namespace CursoMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index(){
+            var pessoa = new CursoMVC.Models.Pessoa{
+                PessoaId = 1,
+                Nome = "Luiz Brandão",
+                Twitter = "@luizbrandaoj"
+            };
+            
+            return View(pessoa);
+        }
+
+        public ActionResult Sobre() {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Lista(Pessoa pessoa){
+            //int PessoaId, string Nome, string Twitter Parametros
+            /*var pessoa = new Pessoa {};
+            pessoa.PessoaId = PessoaId;
+            pessoa.Nome = Nome;
+            pessoa.Twitter = Twitter;*/
+            return View(pessoa);
+        }
+    }
+}
---
+++ CursoMVC/Models/Pessoa.cs
@@ -0,0 +1,9 @@
+﻿namespace CursoMVC.Models
+{
+    public class Pessoa
+    {
+        public int PessoaId { set; get; }
+        public string Nome { set; get; }
+        public string Twitter { set; get; }
+    }
+}
